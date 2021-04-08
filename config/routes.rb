f793a0cc8117cbc557c5b0c8494e6bd848d8c4e3
fr15ky3816 <shota.t.3816@gmail.com>
2021-04-08 16:04:03 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  
-  get 'books/create'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get 'homes/top' => 'homes#top'
+
+  resources :books
+  get '/' => 'homes#top'
   get 'homes/index' => 'homes#index'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
