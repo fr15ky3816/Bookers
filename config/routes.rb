b0a76341b2dee193a97e04579f48c35fd5e348ba
fr15ky3816 <shota.t.3816@gmail.com>
2021-04-11 10:35:16 +0000

    add 4/11

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   resources :books
-  get '/' => 'homes#top'
+  root :to => 'homes#top'
   get 'homes/index' => 'homes#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
